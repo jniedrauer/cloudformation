33255557583146b96486451223b944a037a0bb15
Josiah Niedrauer <jniedrauer@gmail.com>
2018-01-22 00:24:56 -0600

    Update with new resources

--- provisioners/get-ldap-publickeys.sh
+++ provisioners/get-ldap-publickeys.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-ldapsearch \
-    '(&(objectClass=User)(uid='"$1"'))' \
+ldapsearch -LLL \
+    '(&(objectClass=User)(name='"$1"'))' \
     'sshPublicKey' \
     | sed -n '/^ /{H;d};/sshPublicKey:/x;$g;s/\n *//g;s/sshPublicKey: //gp'
