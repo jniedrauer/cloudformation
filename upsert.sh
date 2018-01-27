fbc309dfbfd3a92abc25b7f8338f5da128f6857d
Josiah Niedrauer <jniedrauer@gmail.com>
2018-01-27 13:50:51 -0600

    Add s3 sync and config file

--- upsert.sh
+++ upsert.sh
@@ -5,8 +5,12 @@ set -eux
 stack="${1%/*}"
 wd="$(pwd)"
 
+. cfn.cfg
+
 main() {
     validate_stacks
+    provisioner_sync
+    cfn_sync
 
     if stack_exists >/dev/null 2>&1; then
         update_stack
@@ -19,6 +23,15 @@ validate_stacks() {
     find "$wd/$stack" -type f -name '*yml' -print0 | xargs -0 -I % \
         aws cloudformation validate-template \
         --template-body file://%
+}
+
+provisioner_sync() {
+    aws s3 sync "$PROVISIONER_PATH/" "s3://$CFN_BUCKET/$PROVISIONER_PATH/"
+}
+
+cfn_sync() {
+    aws s3 sync "$stack/" "s3://$CFN_BUCKET/$stack/" \
+        --exclude '*' --include '*.yml'
 }
 
 stack_exists() {
