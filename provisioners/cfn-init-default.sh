40286c2c1004e788abe6739785d0cd3e5259651c
Josiah Niedrauer <jniedrauer@gmail.com>
2018-01-27 15:05:53 -0600

    Update provisioner to use SSM parameter

--- provisioners/cfn-init-default.sh
+++ provisioners/cfn-init-default.sh
@@ -2,63 +2,57 @@
 
 set -eux
 
-main() {
-    yum -y update
+ADMIN_GROUPS=(
+    wheel
+)
 
-    # For debugging only
-    add_user \
-        jniedrauer \
-        'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDZxuzrQfHmOhXXadbDXzcSrNe0pCKx7/DLCYOk2cWgr jniedrauer@homeserver'
+SERVICES=(
+)
+
+PACKAGES=(
+)
+
+main() {
+    IFS=',' read -r -a users <<< "$1"
+    for user in "${users[@]}"; do
+        add_user "$user"
+    done
 
     install_packages
-
-    #join_realm
-    #enable_services
     set_sudoers
-
-    #reboot
 }
 
 install_packages() {
-    packages=(
-        adcli
-        sssd
-        realmd
-        krb5-workstation
-        samba-common-tools
-    )
-    yum -y install "${packages[@]}"
-}
-
-join_realm() {
-    realm join --verbose \
-        -U Administrator@AD.JNIEDRAUER.LOCAL ad.jniedrauer.local
+    test -n "${PACKAGES:-}" \
+        && yum -y install "${PACKAGES[@]}" \
+        || return 0
 }
 
 enable_services() {
-    services=(
-        sssd
-    )
-    for service in "${services[@]}"; do
+    for service in "${SERVICES[@]:-}"; do
         systemctl enable "$service"
         systemctl start "$service"
     done
 }
 
 set_sudoers() {
-    printf '\n%%wheel ALL=(ALL) NOPASSWD: ALL\n' >>/etc/sudoers
-    printf '\n%%Linux\ Admins@ad.jniedrauer.com ALL=(ALL) NOPASSWD: ALL\n' >>/etc/sudoers
+    for group in "${ADMIN_GROUPS[@]:-}"; do
+        (umask 337; touch "/etc/sudoers.d/$group";)
+        printf '%%%s ALL=(ALL) NOPASSWD: ALL\n' "$group" >>"/etc/sudoers.d/$group"
+    done
 }
 
 add_user() {
-    # For debugging only
-    user="$1"
-    pubkey="$2"
+    IFS=';' read -r -a userdetails <<< "$1"
+    user="${userdetails[0]}"
+    pubkeys=("${userdetails[@]:1}")
     getent passwd "$user" >/dev/null 2>&1 \
         || useradd -G wheel "$user"
     su "$user" -c 'mkdir -p ~/.ssh -m 0700'
     su "$user" -c 'umask 077; touch ~/.ssh/authorized_keys'
-    echo "$pubkey" >"/home/$user/.ssh/authorized_keys"
+    for pubkey in "${pubkeys[@]}"; do
+        echo "$pubkey" >>"/home/$user/.ssh/authorized_keys"
+    done
 }
 
-main
+main "$@"
